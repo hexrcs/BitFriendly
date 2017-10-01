c4656e4c0549bd88f29f98bee1fbc52385502b8d
Xiaoru Li <thexrcs@gmail.com>
2017-10-01 20:37:26 +0200

    Rewrite utilizing booleans to make code cleaner
    Known issue: recursive call breaks function return in addOperation().
    See FIXME in if-else-return section of addOperation().

--- src/index.js
+++ src/index.js
@@ -1,152 +1,158 @@
-// factory to create binary numbers with all info attached
+function bitUnit(val) {
+  switch (val) {
+    case 1:
+    case "1":
+    case true:
+      return true;
+    case 0:
+    case "0":
+    case false:
+      return false;
+    default:
+      return undefined;
+  }
+}
 
-function createBinaryNumber({isStrict = true, str, length = str.length}) {
+function bitArray(vals) {
+  let arr = [];
+  for (let val of vals) {
+    arr.push(bitUnit(val));
+  }
+  return arr;
+}
+
+function format(arr = [], width = arr.length) {
+  if (arr.length < width) {
+    let paddingCount = width - arr.length;
+    for (let i = 0; i < paddingCount; ++i) {
+      arr.unshift(false);
+    }
+  } else {
+    let trimmingCount = arr.length - width;
+    for (let i = 0; i < trimmingCount; ++i) {
+      arr.shift();
+    }
+  }
+  return arr;
+}
+
+function operate(arr = [], width = arr.length, altWidth = width) {
   return {
-    isStrict,
-    str: assureLength(str, length),
-    length: assureLength(str, length).length, // if given length is smaller than actual, this will be actual length
-    value: createBitUnitsFromString(assureLength(str, length)),
+    realArr: arr,
+    formattedArr: format(arr, width),
+    altFormattedArr: format(arr, altWidth),
 
-    toString(format = "empty") {
-      let binaryOutput = arrayToString(this.value);
-      switch (format) {
-        case "empty":
-        case 2:
-          return binaryOutput;
-        case 16:
-          return "0x" + parseInt(binaryOutput, 2).toString(16);
-        case 10:
-          return parseInt(binaryOutput, 2).toString(10);
-        default:
-          return "This is not working LOL your format argument is illegal!"
-      }
-    },
+    and(right = []) {
 
-    // don't need to care isStrict first, consider both operands have same length for now
-    // TODO make isStrict work
-
-    add({value}) {
-      let copiedValue = createBitUnitsFromString(this.toString());
-      let result = createBitUnitsFromString(createZeroBitUnits(copiedValue));
-      // 0 + 0 = 0
-      // 0 + 1 = 1
-      // 1 + 0 = 1
-      // 1 + 1 = 0 carry 1
-      return createBinaryNumber({isStrict: this.isStrict, str: arrayToString(result)});
-    },
-
-    and({value}) {
-      let copiedValue = createBitUnitsFromString(this.toString());
-      // initialize an all 0 BitUnits
-      let result = createBitUnitsFromString(createZeroBitUnits(copiedValue));
-      // if both bits in the compared position are 1,
-      // the bit in the resulting binary representation is 1,
-      // otherwise it's 0
-      for (let i = 0; i < copiedValue.length; ++i) {
-        if (copiedValue[i].state === 1 && value[i] === 1)
-          result[i].flip();
-      } // TODO consider when not same length
-      return createBinaryNumber({isStrict: this.isStrict, str: arrayToString(result)});
-    },
-
-    or({value}) {
-      let copiedValue = createBitUnitsFromString(this.toString());
-      let result = createBitUnitsFromString(createZeroBitUnits(copiedValue));
-      // if not both bits are 0, the result is 1.
-      for (let i = 0; i < copiedValue.length; ++i) {
-        if (!(copiedValue[i].state === 0 && value[i] === 0)) {
-          result[i].flip();
-        }
-      } // TODO consider when not same length
-      return createBinaryNumber({isStrict: this.isStrict, str: arrayToString(result)});
-    },
-
-    xor({value}) {
-      let copiedValue = createBitUnitsFromString(this.toString());
-      let result = createBitUnitsFromString(createZeroBitUnits(copiedValue));
-      // 1 if the two bits are different, and 0 if they are the same.
-      for (let i = 0; i < copiedValue.length; ++i) {
-        if (!(copiedValue[i].state === value[i])) {
-          result[i].flip();
-        }
-      } // TODO consider when not same length
-      return createBinaryNumber({isStrict: this.isStrict, str: arrayToString(result)});
-    },
-
-    // 1's complement
-    not() {
-      let copiedValue = createBitUnitsFromString(this.toString());
-      for (let i = 0; i < copiedValue.length; ++i) {
-        copiedValue[i].flip();
-      }
-      return createBinaryNumber({isStrict: this.isStrict, str: arrayToString(copiedValue)});
-    },
-
-    // rotate with carry to the right
-    rcr() {
-      let copiedValue = createBitUnitsFromString(this.toString());
-      let originalLastBit = copiedValue[copiedValue.length - 1];
-      copiedValue.pop();
-      copiedValue.unshift(originalLastBit);
-      // console.log(arrayToString(copiedValue));
-      return createBinaryNumber({isStrict: this.isStrict, str: arrayToString(copiedValue)});
-    },
-
-    idle() {
-      return this;
     }
   }
 }
 
-// if given length is smaller than actual length, this function will do nothing
-const assureLength = (str = "", length = str.length) => {
-  let padding = "";
-  for (let i = 0; i < length - str.length; ++i) {
-    padding = padding + "0";
-  }
-  return padding + str;
-};
-
-const createBitUnitsFromString = (str = "") => {
-  const bitUnits = [];
-  for (let i = 0; i < str.length; ++i) {
-    let newBitUnit = Number(str[i]); // exception (not 0 or 1) handling later
-    bitUnits.push(createBitUnit(newBitUnit));
-  }
-  return bitUnits;
-};
-
-function arrayToString(arr = []) {
-  let output = "";
-  for (let i = 0; i < arr.length; ++i) {
-    output = output + arr[i].toString();
-  }
-  return output;
-}
-
-function createZeroBitUnits(value = []) {
-  let zeroStr = "";
-  for (let i = 0; i < value.length; ++i) {
-    zeroStr = 0 + zeroStr;
-  }
-  return zeroStr;
-}
-
-
-function createBitUnit(initialState = 0) {
-  return {
-    state: initialState,
-    flip() {
-      if (this.state === 0) {
-        return createBitUnit(1);
-      } else return createBitUnit(0);
-    },
-
-    toString() {
-      return this.state;
+function isZero(arr) { // check if the array is all 0/false
+  for (let val of arr) {
+    if (val === true) {
+      return false;
     }
   }
+  return true;
 }
 
-// tests
-// createBinaryNumber({str: 1001}).rcr();
+function addOperation(left = [], right = [], width = left.length) {
+  if (left.length !== right.length) {
+    throw (new Error("Bitwise ADD can only be performed on 2 operands with same length"));
+  }
+  let tempArr = [];
+  let carryArr = [false]; // preset the right most "digit" to 0/false
+
+  for (let i = left.length - 1; i >= 0; --i) { // validate from right to left
+    if (left[i] && right[i]) { // 1 + 1 = 0 carry 1
+      tempArr.unshift(false);
+      carryArr.unshift(true);
+    } else if (!left[i] && !right[i]) { // 0 + 0 = 0 carry 0
+      tempArr.unshift(false);
+      carryArr.unshift(false);
+    } else { // 1 + 0 = 1; 0 + 1 = 1 ===> carry 0
+      tempArr.unshift(true);
+      carryArr.unshift(false);
+    }
+  }
+
+  // FIXME this code below breaks due to recursive call! Too sleepy to figure it out now...
+  // check if the carry array is all zero to determine operation's completion
+  if (!isZero(carryArr)) {
+    tempArr.unshift(false); // align two arrays
+    addOperation(tempArr, carryArr, width);
+  } else {
+    return format(tempArr, width);
+  }
+}
+
+console.log(
+  addOperation([false, true, false, false, false, true, false, false],
+    [true, true, false, true, true, true, false, true])
+);
+
+function andOperation(left = [], right = []) {
+  if (left.length !== right.length) {
+    throw (new Error("Bitwise AND can only be performed on 2 operands with same length"));
+  }
+  let result = [];
+  for (let i = left.length - 1; i >= 0; --i) { // validate from right to left
+    if (left[i] && right[i]) { // if only the i-th values are both true, result is true
+      result.unshift(true);
+    } else {
+      result.unshift(false);
+    }
+  }
+  return result;
+}
+
+function orOperation(left = [], right = []) {
+  if (left.length !== right.length) {
+    throw (new Error("Bitwise OR can only be performed on 2 operands with same length"));
+  }
+  let result = [];
+  for (let i = left.length - 1; i >= 0; --i) { // validate from right to left
+    if (!left[i] && !right[i]) { // if only the i-th values are both false, result is false
+      result.unshift(false);
+    } else {
+      result.unshift(true);
+    }
+  }
+  return result;
+}
+
+function xorOperation(left = [], right = []) {
+  if (left.length !== right.length) {
+    throw (new Error("Bitwise XOR can only be performed on 2 operands with same length"));
+  }
+  let result = [];
+  for (let i = left.length - 1; i >= 0; --i) { // validate from right to left
+    if (left[i] === right[i]) { // if only the i-th values are same, result is true
+      result.unshift(true);
+    } else {
+      result.unshift(false);
+    }
+  }
+  return result;
+}
+
+function notOperation(left = []) {
+  let result = [];
+  for (let i = left.length - 1; i >= 0; --i) { // validate from right to left
+    result.unshift(!left[i]);
+  }
+  return result;
+}
+
+function rcrOperation(left = []) {
+  if (left.length === 0) {
+    return [];
+  }
+  let result = [];
+  for (let i = left.length - 2; i >= 0; --i) { // validate from right to left
+    result.unshift(left[i]);
+  }
+  result.unshift(left[left.length - 1]);
+  return result;
+}
